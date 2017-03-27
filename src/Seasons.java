b78bce4332030ea27b83e46b7be0b798c88f3831
Siyu Kat Wang <siyukw@gmail.com>
2017-03-27 12:33:31 -0700

    month to season

--- src/Seasons.java
+++ src/Seasons.java
@@ -15,5 +15,17 @@ public class Seasons {
     public static int whichSeason(int month) {
         // spring: 3, 4, 5; Summer: 6, 7, 8
         // autumn: 9, 10, 11; Winter: 12, 1, 2
+        if(month == 12 || month == 1 || month == 2) {
+            return 4;
+        } else if(month > 2 && month < 6) {
+            return 1;
+        } else if(month > 5 && month < 9) {
+            return 2;
+        } else if(month >8 && month < 12) {
+            return 3;
+        } else {
+            System.out.println("You are too stupid to use this sorry bye.");
+            return 0;
+        }
     }
 }
