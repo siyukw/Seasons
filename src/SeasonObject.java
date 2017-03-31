b3c83604567493ecc8046bbdd8b89697f714b0fb
Siyu Kat Wang <siyukw@gmail.com>
2017-03-30 23:38:15 -0700

    added theme color for each season

--- src/SeasonObject.java
+++ src/SeasonObject.java
@@ -1,15 +1,25 @@
 import java.awt.*;
 
 // create object for seasons
-// spring: 3, 4, 5; Summer: 6, 7, 8
-// autumn: 9, 10, 11; Winter: 12, 1, 2
+// spring: 1; Summer: 2
+// autumn: 3; Winter: 4
 public class SeasonObject {
     private int season;
+    private Color sColor;
 
     public SeasonObject(int season) {
         DrawingPanel p = new DrawingPanel(1200, 800);
         Graphics g = p.getGraphics();
         this.season = season;
+        if (season == 1) {
+            sColor = new Color(125, 180, 36);
+        } else if (season == 2) {
+            sColor = new Color(22, 100, 31);
+        } else if (season == 3) {
+            sColor = new Color(158, 84, 34);
+        } else {
+            sColor = Color.white;
+        }
     }
 
 
