8ce14870c5904c511e33f5774beaa74c7eeaa1ad
Siyu Kat Wang <siyukw@gmail.com>
2017-03-30 23:33:42 -0700

    added comments in object class

--- src/SeasonObject.java
+++ src/SeasonObject.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 
 // create object for seasons
+// spring: 3, 4, 5; Summer: 6, 7, 8
+// autumn: 9, 10, 11; Winter: 12, 1, 2
 public class SeasonObject {
     private int season;
 
@@ -9,6 +11,8 @@ public class SeasonObject {
         Graphics g = p.getGraphics();
         this.season = season;
     }
+
+
 
 
 }
